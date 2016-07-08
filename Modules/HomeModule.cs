6a408da8796ba78534cdbe0725f27d7a6840073e
Stewart Cole <coleste001@gmail.com>
2016-07-08 11:32:36 -0700

    added path to find word and return the value to the result

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -3,13 +3,20 @@ using Nancy;
 using WordFinder.Objects;
 using System.Collections.Generic;
 
-namespace Finder
+namespace WordFinder
 {
   public class HomeModule : NancyModule
   {
     public HomeModule()
     {
       Get["/"] =_=> View["index.cshtml"];
+      Post["/phrase_search"] =_=> {
+        Finder newFinder = new Finder();
+        newFinder.SetWord(Request.Form["wordSearch"]);
+        newFinder.SetPhrase(Request.Form["phraseSearch"]);
+        newFinder.FindWord();
+        return View["index.cshtml", newFinder.GetInstancesFound()];
+      };
     }
   }
 }
